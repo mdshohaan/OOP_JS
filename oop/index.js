e91fb39c769c83729f9d4f8002dd44b13e282270
mdshohaan <irfanulislamdu@gmail.com>
2025-02-24 18:24:48 +0600

    first commit

---
+++ oop/index.js
@@ -0,0 +1,10 @@
+// RAW Object
+const person ={}
+person.name="Irfan"
+person.skill="JS"
+//  console.log(person);
+ 
+class Person {} // this is template
+const p1 = new Person() // this is instance
+p1.name = "Irfan"
+console.log(p1);
