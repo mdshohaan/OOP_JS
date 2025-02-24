04e35016e7940d038bba494a1dccff7dbbd8e31a
mdshohaan <irfanulislamdu@gmail.com>
2025-02-24 21:20:39 +0600

    chore:methods related

--- oop/methods.js
+++ oop/methods.js
@@ -1,13 +1,15 @@
 class Person {
-  constructor(name,email) {
-    this.name = name
-    this.email = email
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
   }
-  changeName(msg){
-    console.log("send name",this.name);
-    console.log("send email to ",this.email);
-    console.log(msg);
+  changeName(email) {
+  this.email = email
+  }
+  print(){
+    console.log(this);
   }
 }
-const p1 = new Person("ARif",'abc@gmai.com')
-p1.changeName("hello")
+const p1 = new Person("ARif", "abc@gmai.com");
+p1.changeName("xyz@gmial.com");
+p1.print() // Person { name: 'ARif', email: 'xyz@gmial.com' }
