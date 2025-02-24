f002cb2deaec590bd8c3ab24e4606687ecdeaf1c
mdshohaan <irfanulislamdu@gmail.com>
2025-02-25 02:13:48 +0600

    value addedd

--- oop/Getter_Setter2.js
+++ oop/Getter_Setter2.js
@@ -7,11 +7,18 @@ class Person {
   //   return this._name;
   // }
   get name() {
-    return this._name.toUpperCase() // you can handle this function as you want !!
+    return this._name.toUpperCase(); // you can handle this function as you want !! we have endless oppoetunity to handle this
+  }
+  // set name(value) {
+  //   return (this._name = value);
+  // }
+  set name(value) {
+    if(value.toLowerCase() === "islam") return;
+    return (this._name = value);
   }
 }
 const p1 = new Person("SOhan", "abc@gmail.com");
 const p2 = new Person("Islam", "abc@gmail.com");
-p1.name = "somefds";
-console.log(p1.name); // SOHAN
-console.log(p2.name); // ISLAM
+p1.name = "islammmm";
+console.log(p1.name); // ISLAMMMM (SET Function)
+console.log(p2.name); // ISLAM  (GET Function)
