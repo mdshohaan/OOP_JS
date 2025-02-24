485c1b2a2271ccb7cdf68be11103cb7847cbc712
mdshohaan <irfanulislamdu@gmail.com>
2025-02-24 19:48:42 +0600

    first

--- oop/index.js
+++ oop/index.js
@@ -1,10 +1,16 @@
 // RAW Object
-const person ={}
-person.name="Irfan"
-person.skill="JS"
+const person = {};
+person.name = "Irfan";
+person.skill = "JS";
 //  console.log(person);
- 
-class Person {} // this is template
-const p1 = new Person() // this is instance
-p1.name = "Irfan"
-console.log(p1);
+
+class Person {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+}
+const p1 = new Person("IRFANUL"); // this is instance
+const p2 = new Person("ISLAM");
+console.log(p1); // Person { name: 'IRFANUL', skill: 'JS', email: undefined }
+console.log(p2); // Person { name: 'ISLAM', skill: 'JS', email: undefined }
