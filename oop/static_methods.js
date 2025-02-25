98f53785af187266189797e191cca23788738d93
mdshohaan <irfanulislamdu@gmail.com>
2025-02-25 20:59:49 +0600

    chore:static method related more

--- oop/static_methods.js
+++ oop/static_methods.js
@@ -1,12 +1,34 @@
 class Person {
-  constructor(name,email) {
-    this.name = name
-    this.email = email
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
   }
- static  className = "Irfanul Islam"
- static PI = 3.1416
+  // static properties related
+  static className = "Irfanul Islam";
+  static PI = 3.1416;
+
+  // static method related
+  static isValid(age) {
+    return (age) => 20;
+  }
 }
-const p1 = new Person("sohan","abc@gmail.com")
+const p1 = new Person("sohan", "abc@gmail.com");
+
+// static properties related
 console.log(p1.className); // undefined (can't access the value by using this object)
 //*  we can access the value by  using  the class
 console.log(Person.className); // Irfanul Islam
+console.log(Person.PI); // 3.1416
+
+// static method related
+const testUser = {
+  name: "test",
+  email: "test@gmail.com",
+  age: 25,
+};
+let p2 = null;
+
+if (Person.isValid(testUser.age)) {
+  p2 = new Person(testUser.name, testUser.email);
+}
+console.log(p2);
