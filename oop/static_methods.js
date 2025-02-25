76fe12311c02effbdfc2b61ae2c624348954093f
mdshohaan <irfanulislamdu@gmail.com>
2025-02-25 21:09:49 +0600

    more explained

--- oop/static_methods.js
+++ oop/static_methods.js
@@ -11,8 +11,16 @@ class Person {
   static isValid(age) {
     return (age) => 20;
   }
+
+  // equality check
+  static equal(p1, p2) {
+    if (p1.name !== p2.name) return false;
+    if (p1.email !== p2.email) return false;
+    return true;
+  }
 }
 const p1 = new Person("sohan", "abc@gmail.com");
+const p3 = new Person("sohan", "abc@gmail.com");
 
 // static properties related
 console.log(p1.className); // undefined (can't access the value by using this object)
@@ -26,9 +34,15 @@ const testUser = {
   email: "test@gmail.com",
   age: 25,
 };
+
 let p2 = null;
 
 if (Person.isValid(testUser.age)) {
   p2 = new Person(testUser.name, testUser.email);
 }
-console.log(p2);
+console.log(p2); // Person { name: 'test', email: 'test@gmail.com' }
+//  equality check
+console.log(p1.name === p2.name); // false
+console.log(p1.name === p3.name); // true
+//Two object never same but their properties can be same 
+console.log(p1 === p3); // false because of object never be same 
