0fb354aff95384a737375c055c98df85ccdb37ff
mdshohaan <irfanulislamdu@gmail.com>
2025-02-26 19:40:14 +0600

    session more

--- oop/person/UniPerson.js
+++ oop/person/UniPerson.js
@@ -1,8 +1,29 @@
-const Person = require('./Person')
+const Person = require("./Person");
+const _department = Symbol("department");
+const _account = Symbol("account");
 
 class UniPerson extends Person {
-  
-  constructor(parameters) {
-    
+  constructor(id, name) {
+    super(id, name);
+    this[_department] = null;
+    this[_account] = null;
+  }
+  get department() {
+    return this[_department];
+  }
+  set department(value) {
+    this[_department] = value;
+  }
+  get account() {
+    return this[_account];
+  }
+  set account(value) {
+    this[_account] = value;
+  }
+  toString() {
+    super.toString();
+    console.log(`Department:${this[_department]}`);
+    console.log(`Account:${this[_account]}`);
   }
 }
+export default UniPerson;
