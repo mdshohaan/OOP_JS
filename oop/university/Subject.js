4489ae0693b4fce644ad1efe28af730d0c9d6b97
mdshohaan <irfanulislamdu@gmail.com>
2025-02-27 03:51:55 +0600

    subject all done

---
+++ oop/university/Subject.js
@@ -0,0 +1,27 @@
+const _id = Symbol("id");
+const _name = Symbol("name");
+const _credit = Symbol("credit");
+
+class Subject {
+  constructor(id, name, credit) {
+    this[_id] = id;
+    this[_name] = name;
+    this[_credit] = credit || 0;
+  }
+  get id() {
+    return this[_id];
+  }
+  get name() {
+    return this[_name];
+  }
+  set name(value) {
+    this[_name] = value;
+  }
+  get credit() {
+    return this[_credit];
+  }
+  set credit(value) {
+    this[_credit] = value;
+  }
+}
+module.exports = Subject;
