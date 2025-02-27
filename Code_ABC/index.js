652202a98846ca641dfce512dd5fb73c17988264
mdshohaan <irfanulislamdu@gmail.com>
2025-02-28 02:54:59 +0600

    create new file

---
+++ Code_ABC/index.js
@@ -0,0 +1,8 @@
+const computer = function(name,model){
+ this.name = name
+ this.model = model
+}
+const dell = new computer("dell","dl102")
+const hp = new computer("hp","1254")
+console.log(dell);
+console.log(hp);
