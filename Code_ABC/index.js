beb09a5628302418ebeb7b4e9e43050e46228113
mdshohaan <irfanulislamdu@gmail.com>
2025-02-28 03:04:26 +0600

    primary element did that

--- Code_ABC/index.js
+++ Code_ABC/index.js
@@ -1,8 +1,22 @@
-const computer = function(name,model){
- this.name = name
- this.model = model
-}
-const dell = new computer("dell","dl102")
-const hp = new computer("hp","1254")
+// OOP
+const Computer = function (name, model) {
+  this.name = name;
+  this.model = model;
+
+  this.aboutComputer = function () {
+    console.log(`this is the ${this.name} and ${this.model}`);
+  };
+};
+const dell = new Computer("dell", "dk102");
+const hp = new Computer("hp", "1254");
 console.log(dell);
 console.log(hp);
+dell.aboutComputer();
+hp.aboutComputer();
+
+// Procedural way to create an object
+// const obj ={}
+// obj.name ="asus"
+// obj.model = "LP23"
+// console.log(obj);
+//* All above object same to same
