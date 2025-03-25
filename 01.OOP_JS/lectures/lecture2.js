7500f365fbd7c394569d6fece4afc69959843276
mdshohaan <irfanulislamdu@gmail.com>
2025-03-25 23:34:18 +0600

    feat:more explore constructor

--- 01.OOP_JS/lectures/lecture2.js
+++ 01.OOP_JS/lectures/lecture2.js
@@ -1,8 +1,11 @@
 var book = [{ name: "Javascript " }, { name: "React " }];
 
-function Person() {
-  console.log("I have Constructed......");
+function Person(name, email) {
+  //  console.log(this); // this means refer to Object
+  this.name = name;
+  this.email = email;
 }
-const p1 = new Person();
+const p1 = new Person("irfan", "abc@gmail.com");
+console.log(p1.name);
 
 console.log(p1);
