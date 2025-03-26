4216026e2fc44f5e8f09ba539442d1c74343b979
mdshohaan <irfanulislamdu@gmail.com>
2025-03-26 11:27:59 +0600

    feat:Constructor object

--- 01.OOP_JS/lectures/lecture2.js
+++ 01.OOP_JS/lectures/lecture2.js
@@ -4,9 +4,14 @@ function Person(name, email) {
   //  console.log(this); // this means refer to Object
   this.name = name;
   this.email = email;
+  this.print = function () {
+    console.log(this.name);
+  };
 }
 const p1 = new Person("irfan", "abc@gmail.com");
 const p2 = new Person("irfanul islam", "xyz@gmail.com");
 const people = [p1, p2];
 
-console.log(people);
+people.forEach((person) => {
+  person.print();
+});
