089ccf47c886f2e825aa521b0c0cb8b5a0ea0186
mdshohaan <irfanulislamdu@gmail.com>
2025-03-26 12:20:46 +0600

    feat;Add the constructor explore more

--- 01.OOP_JS/lectures/lecture2.js
+++ 01.OOP_JS/lectures/lecture2.js
@@ -1,3 +1,10 @@
+// * @Title- Constructor
+// To create an object type we use an object constructor function.
+// The value of this will become the new object when a new object is created.
+//* In JavaScript, constructors can be defined in two main ways:
+// Function Constructor (Before ES6)
+// Class Constructor (ES6 and Beyond)
+
 var book = [{ name: "Javascript " }, { name: "React " }];
 
 function Person(name, email) {
@@ -15,3 +22,29 @@ const people = [p1, p2];
 people.forEach((person) => {
   person.print();
 });
+
+function Person1(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+const p3 = new Person1("MD", "Islam");
+// Adding a property to a created object is easy:
+p3.Bangladeshi = true;
+p3;
+
+// To add a new property, you must add it to the constructor function prototype:
+Person1.prototype.nationality = "English";
+console.log(p3);
+
+// Use object literals {} instead of new Object().
+
+// Use array literals [] instead of new Array().
+
+// Use pattern literals /()/ instead of new RegExp().
+
+// Use function expressions () {} instead of new Function().
+const arr = new Array();
+const arr1 = [];
+console.log(arr === arr1); // false
